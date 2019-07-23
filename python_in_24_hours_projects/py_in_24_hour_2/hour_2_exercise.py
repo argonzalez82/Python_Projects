202e4b8d9635ef1ed482992d47f381ca187c0e91
argonzalez82 <48436472+argonzalez82@users.noreply.github.com>
2019-07-23 03:21:19 -0700

    Update hour_2_exercise.py

--- python_in_24_hours_projects/py_in_24_hour_2/hour_2_exercise.py
+++ python_in_24_hours_projects/py_in_24_hour_2/hour_2_exercise.py
@@ -1,2 +1,23 @@
-# hour_1_exercise 
+# hour_2_exercise
+# Chapter 2 page26
+# small program to figure total price including discount
 
+supplies_cost =10
+print("supplies_cost = $",supplies_cost) 
+
+discount =supplies_cost * 30 / 100
+print("discount =  $",discount)
+print("*****" * 3)
+
+shipping_cost =7.50
+
+subtotal = (supplies_cost - discount + shipping_cost)
+print("$",subtotal)
+
+state_tax = (subtotal * 0.05)
+print("$",state_tax)
+
+print("******" * 3)
+
+total = (subtotal + state_tax)
+print("$",total)
