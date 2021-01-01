94853f1e42998d5169195f05d9ca1f46535322e5
argonzalez82 <argonzalez82@gmail.com>
2021-01-01 14:42:32 -0700

    Initial commit for the hackerrank directory

---
+++ HackerRank/HRchallengePractice.py
@@ -0,0 +1,23 @@
+	#Declare 3 variables a, b, c
+
+	#Use the  operator to perform the following operations:
+	#Print the sum of  plus your int variable on a new line.
+	#Print the sum of  plus your double variable to a scale 
+	#of one decimal place on a new line.
+	#Concatenate  with the string you read as input and 
+	#print the result on a new line.
+
+		i = 4
+		d = 4.0
+		s = 'HackerRank '
+	# Declare second integer, double, and String variables.
+		a = 3
+		b = 2.8
+		c = "is the best place to learn and practice coding!"
+	# Print the sum of both integer variables on a new line.
+		print(i + a)
+	# Print the sum of the double variables on a new line.
+		print(d + b)
+	# Concatenate and print the String variables on a new line
+	# The 's' variable above should be printed first.
+		print(s + c)
